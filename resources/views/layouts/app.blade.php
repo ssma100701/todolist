ef20d101e7d36d8cfa6f93716b1abf42dbbae531
LAPTOP-COK9CFVT\ssma1 <ssma100701@gmail.com>
2018-12-04 11:54:15 +1100

    index page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Todolist</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('layouts.footer')
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer id="footer" class="text-center">
+    <p>Copyright &copy; 2018 Todolist</p>
+</footer>
