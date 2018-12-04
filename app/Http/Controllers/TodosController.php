3bd5a4b18280fe42866d55b5c71efdf9f13ee14c
LAPTOP-COK9CFVT\ssma1 <ssma100701@gmail.com>
2018-12-04 13:59:10 +1100

    add Delete

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -47,7 +47,7 @@ class TodosController extends Controller
         // Create Todo
         Todo::create($todo);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Todo Saved');
 
     }
 
@@ -71,7 +71,8 @@ class TodosController extends Controller
      */
     public function edit($id)
     {
-        //
+        $todo = Todo::find($id);
+        return view('todos.edit')->with('todo', $todo);
     }
 
     /**
@@ -83,7 +84,14 @@ class TodosController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $todo = Todo::find($id);
+        $todo->text = $request->input('text');
+        $todo->body = $request->input('body');
+        $todo->due = $request->input('due');
+
+        $todo->save();
+
+        return redirect('/')->with('success', 'Todo Updated');
     }
 
     /**
@@ -94,6 +102,9 @@ class TodosController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return redirect('/')->with('success', 'Todo Deleted');
     }
 }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -6,4 +6,10 @@
     <div class="badge badge-danger">{{ $todo->due }}</div>
     <hr>
     <h3>{{ $todo->body }}</h3>
+    <br><br>
+    <a href="/todo/{{ $todo->id }}/edit" class="btn btn-secondary">Edit</a>
+    {!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'float-right']) !!}
+        {{ Form::hidden('_method','DELETE') }}
+        {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+    {!! Form::close() !!}
 @endsection
